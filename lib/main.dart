7b8963426374547b448ac04b01d4ff4ceb99be2b
Muhammed Bilal <bilalmuhammed363@gmail.com>
2023-06-02 21:08:25 +0530

    added google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,9 +10,14 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily:
+            GoogleFonts.montserrat().fontFamily,
+        primarySwatch: Colors.red,
+      ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      home: const Scaffold(
         body: Center(
           child: Text('Hello World'),
         ),
