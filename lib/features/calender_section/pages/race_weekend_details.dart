cf71ff3595aaf05163832ed46dc0a3c2fe43c716
Muhammed Bilal <bilalmuhammed363@gmail.com>
2023-06-06 17:53:59 +0530

    partially completed event page

--- lib/features/calender_section/pages/race_weekend_details.dart
+++ lib/features/calender_section/pages/race_weekend_details.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pit_box/core/constant/colors.dart';
 import 'package:pit_box/core/constant/size.dart';
+import 'package:pit_box/core/constant/sized_box.dart';
 import 'package:pit_box/core/icons/box_icon_icons.dart';
+import 'package:pit_box/features/championship_section/presentation/pages/driver_details.dart';
 import 'package:pit_box/utils/widgets/app_bar_widget.dart';
 
 class RaceWeekendDetails extends StatelessWidget {
@@ -106,138 +108,229 @@ class RaceWeekendDetails extends StatelessWidget {
                 decoration: BoxDecoration(
                     color: Theme.of(context).canvasColor,
                     borderRadius: BorderRadius.circular(bRadius10)),
-                child: Column(
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(bRadius10),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                '1991',
-                                style: TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.w900),
-                              ),
-                              Text(
-                                'First Grand Prix',
-                                style: TextStyle(
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w600,
-                                    color: colorwhite.withOpacity(0.5)),
-                              )
-                            ],
+                    SizedBox(
+                      width: size.width * 0.35,
+                      height: size.width * 0.4,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: bRadius10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  '1991',
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w900),
+                                ),
+                                Text(
+                                  'First Grand Prix',
+                                  style: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w600,
+                                      color: colorwhite.withOpacity(0.5)),
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(bRadius10),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                '66',
-                                style: TextStyle(
-                                    fontSize: 24, fontWeight: FontWeight.w900),
-                              ),
-                              Text(
-                                'Number of Laps',
-                                style: TextStyle(
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w600,
-                                    color: colorwhite.withOpacity(0.5)),
-                              )
-                            ],
+                          Padding(
+                            padding: const EdgeInsets.only(top: bRadius10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      '307.236',
+                                      style: TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.w900),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.only(top: 6),
+                                      child: Text(
+                                        'Km',
+                                        style: TextStyle(
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  'Race Distance',
+                                  style: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w600,
+                                      color: colorwhite.withOpacity(0.5)),
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(bRadius10),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    '307.236',
-                                    style: TextStyle(
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.w900),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(top: 6),
-                                    child: Text(
-                                      'Km',
-                                      style: TextStyle(
-                                          fontSize: 10,
-                                          fontWeight: FontWeight.w500),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                'Race Distance',
-                                style: TextStyle(
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w600,
-                                    color: colorwhite.withOpacity(0.5)),
-                              )
-                            ],
+                    SizedBox(
+                      width: size.width * 0.35,
+                      height: size.width * 0.4,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: bRadius10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  '66',
+                                  style: TextStyle(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.w900),
+                                ),
+                                Text(
+                                  'Number of Laps',
+                                  style: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w600,
+                                      color: colorwhite.withOpacity(0.5)),
+                                )
+                              ],
+                            ),
                           ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(bRadius5),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text(
-                                    '5.513',
-                                    style: TextStyle(
-                                        color: colorgreen,
-                                        fontSize: 24,
-                                        fontWeight: FontWeight.w900),
-                                  ),
-                                  Padding(
-                                    padding: EdgeInsets.only(top: 6),
-                                    child: Text(
-                                      'Km',
+                          Padding(
+                            padding: const EdgeInsets.only(top: bRadius10),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      '5.513',
                                       style: TextStyle(
-                                          fontSize: 10,
-                                          fontWeight: FontWeight.w500),
+                                          color: colorgreen,
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.w900),
                                     ),
-                                  ),
-                                ],
-                              ),
-                              Text(
-                                'Circuit Length',
-                                style: TextStyle(
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w600,
-                                    color: colorwhite.withOpacity(0.5)),
-                              ),
-                            ],
+                                    Padding(
+                                      padding: EdgeInsets.only(top: 6),
+                                      child: Text(
+                                        'Km',
+                                        style: TextStyle(
+                                            fontSize: 10,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Text(
+                                  'Circuit Length',
+                                  style: TextStyle(
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w600,
+                                      color: colorwhite.withOpacity(0.5)),
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ],
                 ),
               ),
             ),
+            hBox5,
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const DriverDetails(),
+                    ));
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.only(left: bRadius25, right: bRadius10),
+                width: size.width * 0.9,
+                height: size.width * 0.2,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).canvasColor,
+                    borderRadius: BorderRadius.circular(bRadius10)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      flex: 4,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '1:16.330',
+                            style: TextStyle(
+                                color: colorgreen,
+                                fontSize: 22,
+                                fontWeight: FontWeight.w900),
+                          ),
+                          Text(
+                            'Lap Record',
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w600,
+                                color: colorwhite.withOpacity(0.5)),
+                          )
+                        ],
+                      ),
+                    ),
+                    wBox20,
+                    Flexible(
+                      flex: 5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            'Charles Leclerc',
+                            style: TextStyle(
+                                color: colorwhite,
+                                fontSize: 15,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          hBox5,
+                          Text(
+                            '2019',
+                            style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.w600,
+                                color: colorwhite.withOpacity(0.5)),
+                          )
+                        ],
+                      ),
+                    ),
+                    const Flexible(
+                      flex: 3,
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: bRadius10),
+                        child: Icon(BoxIcon.arrow_right),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       ),
